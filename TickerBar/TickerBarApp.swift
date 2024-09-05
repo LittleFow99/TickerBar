edd437dc47a3658ecf1f2dec5df52e1b5d06464f
Thomas Paolino <tpao24@Outlook.com>
2024-09-05 19:33:07 -0400

    Initial Commit

---
+++ TickerBar/TickerBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickerBarApp.swift
+//  TickerBar
+//
+//  Created by Thomas Paolino on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TickerBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
